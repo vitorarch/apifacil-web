7a5c314672d3ff72d6e8c4cc58a56b892f89c061
Alexandre Bernardes <abernardes11192@gmail.com>
2023-09-23 18:56:55 -0300

    :construction: Criando a branch develop

--- crm-api-integrator-web/src/components/features/passRecovery/PasswordReset.css.ts
+++ crm-api-integrator-web/src/components/features/passRecovery/PasswordReset.css.ts
@@ -2,7 +2,7 @@ const container = " flex box-border justify-center mt-[20vh]";
 const card = " p-8 w-[600px] flex flex-col items-center";
 const header = " w-full text-center mb-5";
 const limit = " w-[350px]"
-const passwords = " w-full flex flex-col items-center gap-6 mt-5";
+const passwords = " w-full flex flex-col items-center gap-6 mt-5 ";
 
 const passwordResetCss = {
   container,
